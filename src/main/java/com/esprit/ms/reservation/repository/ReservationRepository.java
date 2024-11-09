fc4e0471dfdb287444792b191a86176880e452d1
Eya-Benhouria <benhouriaeya@gmail.com>
2024-11-09 15:52:27 +0100

    new config

--- src/main/java/com/esprit/ms/reservation/repository/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.esprit.ms.microservicemesssagerie.repository;
-
-import com.esprit.ms.microservicemesssagerie.entity.Reservation;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservationRepository extends CrudRepository<Reservation,Long> {
-}
